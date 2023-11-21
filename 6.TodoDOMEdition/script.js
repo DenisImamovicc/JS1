841af6c585575c74897d3c700b2d0381b5961a52
Denis Imamovic <Danielarbete@protonmail.com>
2023-11-21 15:02:45 +0100

    fixed ul click event handler not working with btn.Cause:🚮

--- 6.TodoDOMEdition/script.js
+++ 6.TodoDOMEdition/script.js
@@ -1,115 +1,95 @@
 const todolistEl = document.querySelector("#todolist");
 const formCreateTodoEl = document.querySelector("#formCreateTodo");
 const inputNewTodoTitleEl = document.querySelector("#inputNewTodoTitle");
+const deleteTodoBtn =  document.querySelector("#deleteBtn")
 
 // List of todos
 const todos = [
-	{
-		title: "Eat",
-		completed: false,
-	},
-	{
-		title: "Code",
-		completed: true,
-	},
-	{
-		title: "Sleep",
-		completed: false,
-	},
-	{
-		title: "Take over the world",
-		completed: false,
-	},
+  {
+    title: "Eat",
+    completed: false,
+  },
+  {
+    title: "Code",
+    completed: true,
+  },
+  {
+    title: "Sleep",
+    completed: false,
+  },
+  {
+    title: "Take over the world",
+    completed: false,
+  },
 ];
 
 formCreateTodoEl.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-	e.preventDefault();
+  const newTodoTitle = inputNewTodoTitleEl.value;
 
-	const newTodoTitle = inputNewTodoTitleEl.value;
+  if (newTodoTitle === "") {
+    return alert("Lazy boy!,you can't have nothing todo.*whipped noises*");
+  }
 
-	if (newTodoTitle === "") {
-		return alert("Lazy boy!,you can't have nothing todo.*whipped noises*");
-	}
+  const newTodo = {
+    title: newTodoTitle,
+    completed: false,
+  };
 
-	const newTodo = {
-		title: newTodoTitle,
-		completed: false,
-	}
+  todos.push(newTodo);
 
-	todos.push(newTodo);
+  renderTodos();
 
-
-	renderTodos();
-
-	
-	inputNewTodoTitleEl.value = "";
+  inputNewTodoTitleEl.value = "";
 });
 
-todolistEl.addEventListener("click", (e) => {
-  const chosenTodo = e.target
-  handleClassnameAssign(chosenTodo,"completed")
+todolistEl.addEventListener("click", (e) => toggleTodoCompletion(e.target))
 
-   todos.find((todo) =>{
-      if(todo.title === chosenTodo.innerText) {
-        return todo.completed = !todo.completed
-      }
-   })
-  console.log(e,todos);
-});
-
-const handleClassnameAssign = (el,classname) => {
+const handleClassnameAssign = (el, classname) => {
   return el.classList[1]
     ? el.classList.remove(classname)
     : el.classList.add(classname);
 };
 
+const toggleTodoCompletion = (chosenTodo) => {
+  const chosenTodoTitle = chosenTodo.childNodes[0].textContent.trim()
+   todos.find((todo) => {
+    if (todo.title === chosenTodoTitle) {
+      handleClassnameAssign(chosenTodo, "completed");
+      return (todo.completed = !todo.completed);
+    }
+  });
+};
 
 const renderTodos = () => {
-	todolistEl.innerText = "";
-  const sortedTodos = sortTodosAlphabetically(todos)
+  todolistEl.innerText = "";
+  const sortedTodos = sortTodosAlphabetically(todos);
 
-	sortedTodos.forEach((todo) => {
-		/*
+  sortedTodos.forEach((todo) => {
+
 		const cssCompleted = todo.completed ? "completed" : "";
 
 		todolistEl.innerHTML += `
-			<li class="list-group-item ${cssCompleted}">
+			<li class="list-group-item ${cssCompleted}" id="todo">
 				${todo.title}
+        <button class="" id="deleteBtn">🚮</button>
 			</li>
 		`;
-		*/
-
-		// create a new li element
-		const todoEl = document.createElement("li");
-		todoEl.innerText = todo.title;
-		todoEl.classList.add("list-group-item");
-
-		if (todo.completed) {
-			todoEl.classList.add("completed");
-		}
-
-		// append the new li element to the ul element
-		todolistEl.append(todoEl);
-	});
-}
+  });
+};
 
 const sortTodosAlphabetically = (todolist) => {
-  return todolist.sort((a,b) => {
-      if (a.title.toLowerCase() < b.title.toLowerCase()) {
-        return -1
-      } 
-      else if (a.title < b.title) {
-        return 1
-      }else{
-        return 0   
-      } 
-  })
-}
+  return todolist.sort((a, b) => {
+    if (a.title.toLowerCase() < b.title.toLowerCase()) {
+      return -1;
+    } else if (a.title < b.title) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+};
 
-const handleTodoCompletion = () => {
-
-}
 
 renderTodos();
-console.log()
