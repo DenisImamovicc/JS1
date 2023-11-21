8f08e4d4ec967141f52622b205314bd7dbc951c5
Denis Imamovic <Danielarbete@protonmail.com>
2023-11-21 12:32:19 +0100

    done with step one

---
+++ 6.TodoDOMEdition/Readme.md
@@ -0,0 +1,16 @@
+## Workshop - Todos
+ 
+### Steg 1 - Sortera todos
+Lägg till funktionalitet som sorterar listan av todos alfabetiskt.
+Glöm inte sortera om listan efter användaren lagt till en ny todo!
+ 
+### Steg 2 - Markera en todo som avklarad
+Lägg till funktionalitet som markerar en todo som avklarad när man klickar på den. Använd vår nya kompis .find() för att hitta rätt todo, ändra desscompleted till true och rendera om listan.
+ 
+Använd event-bubbling, dvs sätt click-handlern på ul!
+ 
+### Steg 2.5 - Toggla en todo
+En todo ska kunna växla mellan att vara avklarad och ej klar. Dvs om den är true ska den ändras till false, och om den är false ska den ändras till true.
+ 
+### ⭐️ Steg 3 - Radera en todo
+Lägg till funktionalitet för att radera en todo när man klickar på en Delete-knapp (som ska finnas på varje todo).
---
+++ 6.TodoDOMEdition/script.js
@@ -0,0 +1,94 @@
+const todolistEl = document.querySelector("#todolist");
+const formCreateTodoEl = document.querySelector("#formCreateTodo");
+const inputNewTodoTitleEl = document.querySelector("#inputNewTodoTitle");
+
+// List of todos
+const todos = [
+	{
+		title: "Eat",
+		completed: false,
+	},
+	{
+		title: "Code",
+		completed: true,
+	},
+	{
+		title: "Sleep",
+		completed: false,
+	},
+	{
+		title: "Take over the world",
+		completed: false,
+	},
+];
+
+formCreateTodoEl.addEventListener("submit", (e) => {
+
+	e.preventDefault();
+
+	const newTodoTitle = inputNewTodoTitleEl.value;
+
+	if (newTodoTitle === "") {
+		return alert("Lazy boy!,you can't have nothing todo.*whipped noises*");
+	}
+
+	const newTodo = {
+		title: newTodoTitle,
+		completed: false,
+	}
+
+	todos.push(newTodo);
+
+
+	renderTodos();
+
+	
+	inputNewTodoTitleEl.value = "";
+});
+
+
+const renderTodos = () => {
+	todolistEl.innerText = "";
+  const sortedTodos = sortTodosAlphabetically(todos)
+	sortedTodos.forEach((todo) => {
+		/*
+		const cssCompleted = todo.completed ? "completed" : "";
+
+		todolistEl.innerHTML += `
+			<li class="list-group-item ${cssCompleted}">
+				${todo.title}
+			</li>
+		`;
+		*/
+
+		// create a new li element
+		const todoEl = document.createElement("li");
+		todoEl.innerText = todo.title;
+		todoEl.classList.add("list-group-item");
+
+		if (todo.completed) {
+			todoEl.classList.add("completed");
+		}
+
+		// append the new li element to the ul element
+		todolistEl.append(todoEl);
+	});
+}
+
+const sortTodosAlphabetically = (todolist) => {
+  return todolist.sort((a,b) => {
+      if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1
+      } 
+      else if (a.title < b.title) {
+        return 1
+      }else{
+        return 0   
+      } 
+  })
+}
+
+// Render the initial representation of the todos-array
+renderTodos();
+
+console.log()
