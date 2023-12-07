172bc1ae4ea37da08c9bf6a8423e5fa72d5cd12a
Denis Imamovic <Danielarbete@protonmail.com>
2023-12-07 15:41:03 +0100

    Refactor code

--- 9.Weather_app/assets/scripts/app.js
+++ 9.Weather_app/assets/scripts/app.js
@@ -1,10 +1,41 @@
-/**
- * 🌧️.
- *
- */
+const popupMssg = document.querySelector("#alert");
+const loadingIcon = document.querySelector("#loadingIcon");
+const forecast = document.querySelector("#forecast")
+
+document.querySelector("#search-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const city = e.target.query.value.trim();
+
+  if (city.length < 3) {
+    return alert("Please enter at least 3 chars")
+  }
+
+  try {
+    forecast.className = "card text-black hide";
+    loadingIcon.className = "spinner-border";
+    const data = await getCurrentWeather(city);
+    loadingIcon.className = "spinner-border hide";
+    renderCurrentWeather(data);
+    forecast.className = "card text-black";
+  } catch (error) {
+    loadingIcon.className = "spinner-border hide";
+    renderErrMssg(city);
+  }
+});
+
+const renderErrMssg = (city) => {
+  const ErrMssgDelay = 3000;
+  popupMssg.className = "alert alert-danger text-center";
+  popupMssg.innerText = `${city} not found.Pls try again or check your spelling`;
+  setTimeout(
+    () => (popupMssg.className = "alert alert-danger text-center hide"),
+    ErrMssgDelay
+  );
+};
+
 const renderCurrentWeather = (conditions) => {
-  // console.log()
-  document.querySelector("#forecast").innerHTML = `
+
+  forecast.innerHTML = `
 		<img src="${checkTimeOfDay(conditions)}" class="card-img-top">
         <span class="text-start ms-2">Updated by: ${checkWeatherUpdateTime(
           conditions
@@ -26,67 +57,23 @@ const renderCurrentWeather = (conditions) => {
 				<span id="windspeed">${conditions.wind.speed}</span>
 				m/s
 			</p>
-            <ul class="conditions">
-                <li><img src="https://openweathermap.org/img/wn/${
-                  conditions.weather[0].icon
-                }@2x.png"  alt="${conditions.weather[0].main}" title="${
-    conditions.weather[0].description
-  }" id="weatherRelations"></li>
-            </ul>
+        <ul class="conditions">
+          <li>
+            <img src="https://openweathermap.org/img/wn/${conditions.weather[0].icon}@2x.png"
+              alt="${conditions.weather[0].main}"
+              title="${conditions.weather[0].description}" 
+              id="weatherRelations">
+          </li>
+        </ul>
 		</div>
 	`;
 };
 
 const checkTimeOfDay = (data) => {
   const currTime = Math.floor(Date.now() / 1000);
-  console.log(data.sys.sunrise);
-  if (currTime > data.sys.sunrise && currTime < data.sys.sunset) {
-    return "assets/images/day.svg";
-  } else {
-    return "assets/images/night.svg";
-  }
+  return currTime > data.sys.sunrise && currTime < data.sys.sunset
+    ? "assets/images/day.svg"
+    : "assets/images/night.svg";
 };
 
-const checkWeatherUpdateTime = (data) => {
-  return new Date(data.dt * 1000).toUTCString();
-};
-
-// Listen for when the user wants to get weather conditions for a city
-document.querySelector("#search-form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const city = e.target.query.value.trim();
-
-  // Make sure input is at least somewhat valid
-  if (city.length < 3) {
-    alert("Please enter at least 3 chars");
-    return;
-  }
-
-  console.log(`Searching for city "${city}"`);
-  document.querySelector("#forecast").className = "card text-black hide";
-  try {
-    document.querySelector("#loadingIcon").className = "spinner-border";
-    const data = await getCurrentWeather(city);
-    document.querySelector("#loadingIcon").className = "spinner-border hide";
-
-    renderCurrentWeather(data);
-    console.log(`Current weather conditions in "${city}":`, data);
-    document.querySelector("#forecast").className = "card text-black";
-  } catch (error) {
-    console.log(error);
-    document.querySelector("#loadingIcon").className = "spinner-border hide";
-    renderErrMssg(city);
-  }
-});
-
-const renderErrMssg = (city) => {
-  const ErrMssgDelay = 3000;
-  document.querySelector("#alert").className = "alert alert-danger text-center";
-  document.querySelector(
-    "#alert"
-  ).innerText = `${city} not found.Pls try again`;
-  setTimeout(() => {
-    document.querySelector("#alert").className =
-      "alert alert-danger text-center hide";
-  }, ErrMssgDelay);
-};
+const checkWeatherUpdateTime = (data) => new Date(data.dt * 1000).toUTCString();
