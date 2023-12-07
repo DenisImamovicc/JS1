91f7bb06c0cdaf79a8c8c17fc256fe7f9e692eee
Denis Imamovic <Danielarbete@protonmail.com>
2023-12-07 13:27:52 +0100

    Workshop for today.Done w 1 and 2

---
+++ 9.Weather_app/assets/scripts/app.js
@@ -0,0 +1,60 @@
+/**
+ * 🌧️.
+ *
+ */
+const renderCurrentWeather = (conditions) => {
+	document.querySelector("#forecast").innerHTML = `
+		<img src="assets/images/forecast-banner.png" class="card-img-top">
+		<div class="card-body">
+			<h5 class="card-title" id="location">
+				<span id="city">${conditions.name}</span>,
+				<span id="country">${conditions.sys.country ?? "N/A"}</span>
+			</h5>
+			<p class="temp">
+				<span id="temperature">${conditions.main.temp}</span>
+				&deg;C
+			</p>
+			<p class="humidity">
+				<span id="humidity">${conditions.main.humidity}</span>
+				&percnt; humidity
+			</p>
+			<p class="wind">
+				<span id="windspeed">${conditions.wind.speed}</span>
+				m/s
+			</p>
+		</div>
+	`;
+}
+
+// Listen for when the user wants to get weather conditions for a city
+document.querySelector("#search-form").addEventListener("submit", async (e) => {
+	e.preventDefault();
+	const city = e.target.query.value.trim();
+
+	// Make sure input is at least somewhat valid
+	if (city.length < 3) {
+		alert("Please enter at least 3 chars");
+		return;
+	}
+
+	console.log(`Searching for city "${city}"`);
+    document.querySelector("#forecast").className = "card text-black hide"
+    try {
+        const data = await getCurrentWeather(city);
+        renderCurrentWeather(data)
+        console.log(`Current weather conditions in "${city}":`, data);
+        document.querySelector("#forecast").className = "card text-black"
+    } catch (error) {
+        renderErrMssg(city)
+    }
+});
+
+
+const renderErrMssg = (city) => {
+    const ErrMssgDelay = 3000
+    document.querySelector("#alert").className = "alert alert-danger text-center"
+    document.querySelector("#alert").innerText = `${city} not found.Pls try again`
+    setTimeout(()=>{
+        document.querySelector("#alert").className = "alert alert-danger text-center hide"
+    },ErrMssgDelay)
+}
---
+++ 9.Weather_app/assets/scripts/owapi.js
@@ -0,0 +1,29 @@
+const API_KEY = "ac714feaf33d10484dea8fd638e75777"
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+
+
+const FAKE_SLOW_API = true;
+const FAKE_SLOW_API_DELAY = 3000;
+
+/**
+ * Get current weather for city from OpenWeatherMap API
+ * @param {string} city
+ */
+const getCurrentWeather = async (city) => {
+	// get current weather conditions
+	const response = await fetch(`${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`);
+
+	// check if response is ok
+	if (!response.ok) {
+		throw new Error(`${response.status} ${response.statusText}`); // 404 Not Found
+	}
+
+	// convert response from JSON
+	const data = await response.json();
+
+	// fake a slow api
+	FAKE_SLOW_API && await new Promise(r => setTimeout(r, FAKE_SLOW_API_DELAY));
+	// return current weather conditions
+	return data;
+}
